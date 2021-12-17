32f5ec79610b674c9c464eb254f11e651f32d5c6
muratcan döngel <muratcan_dongel@hotmail.com>
2021-12-17 15:32:28 +0300

    Add project files.

---
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -0,0 +1,178 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+
+namespace UdemyRabbitMQ.subscriber
+{
+    #region Notlar
+    //Subscriber(Yayınlanan) tarafında kuyruk oluşturma zorunlu değildir.(QueueDeclare Metodu için)
+    //Publisher(Yayınlayan) tarafında belirtilen kuyruk varsa hiçbir işlem yapmaz.
+    //Yoksa belirtilen kuyruğu oluşturur. Eğer 2 tarafta tanımlı olup boolean özellikler farklı tanımlanırsa sistem hata verir.
+    //Kuyruk sistem kapansa bile kayıt eder, silinmez.
+    //AutoAck: Kuyruktaki mesajı silme durumunu belirler.True ise mesajı ilettiğinde kuyruk silinir. False ise silinmez.
+    //Queue Bind(Metot): uygulama ayağa kalktığında kuyruk oluşacak. Kapandığında silinecek.
+    //BasicQos(Metot): Mesajların subscriber(Yayınlanan) alana kaçar tane gideceği belirlenir.
+    //Prefetch Size: Gönderilecek mesajların boyutu belirlenir. 0 verilirse gelen mesajların boyutuna bakmaz.
+    //Prefetch Count: subscriber(Yayınlanan) alana 1 seferde kaç kuyruğun(mesajların) iletileceği belirlenir.
+    //Global: Gönderilecek mesajların verilen mesaj gönderme sayısını parçalı veya tek seferde gönderimi belirlenir.
+    //True ise subscriber(Yayınlanan) alanlara mesaj sayısı toplamı kadar iletilir.(Ör. 6 adet verilmişse 2 subscriber varsa her birine 6 6 olarak iletilir.)
+    //False ise subscriber(Yayınlanan) alanlarla mesaj sayısı eşit şekilde dağıtılır.(Ör. 6 adet verilmişse 2 subscriber varsa birine 3 diğerine 3 olarak iletilir.)
+    //BasicAck(Metot): Gelen mesajı silmek için yazılan metot
+    //DeliveryTag: RabbitMQ ya hangi Tag ile ulaştırılan ilgili Tag üzerinden mesaj silinir.
+    //Multiple: Gelen mesajın RabbitMQ ya iletilme durumu belirler. True ise iletir False ise iletmez.
+    #endregion
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Bağlantı Kurma Aşaması
+            //RabbitMQ ile bağlantı kurulması için ConnectionFactory nesnesi eklendi.
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://zxuhuhnz:uwT3kF1kowQ8859E7O4WgNOon-iqc9VJ@tiger.rmq.cloudamqp.com/zxuhuhnz"); //AMQP URL alındı
+
+            //Eklenen nesne üzerinden RabbitMq Cloud hesabından AMQP URL alınarak bağlantı kuruldu.
+            using var connection = factory.CreateConnection();
+
+            //Oluşturulacak kuyruk için bir kanal(model) yaratıldı.
+            var channel = connection.CreateModel();
+            #endregion
+
+            #region Basit Kuyruk Oluşturma(Exchange Olmadan) 1
+            //var consumer = new EventingBasicConsumer(channel);
+            //channel.BasicConsume("hello-queue", false, consumer);
+            //consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            //{
+            //Byte türünden gelen mesaj string ile karşılandı.
+            //   var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            //   Console.WriteLine("Gelen Mesaj:" + message);
+            //}
+            #endregion
+
+            #region Basit Kuyruk Oluşturma(Exchange Olmadan) 2
+            //channel.BasicQos(0, 1, false);
+            //var consumer = new EventingBasicConsumer(channel);
+            //channel.BasicConsume("hello-queue", false, consumer);
+            //consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            //{
+            //   var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            //   Thread.Sleep(1500);
+            //   Console.WriteLine("Gelen Mesaj:" + message);
+            //   channel.BasicAck(e.DeliveryTag, false);
+            //}
+            #endregion
+
+            #region Fanout Exchange
+            //var randomQueueName = channel.QueueDeclare().QueueName;
+            //channel.QueueBind(randomQueueName, "logs-fanout", "", null);
+            //channel.BasicQos(0, 1, false);
+            //var consumer = new EventingBasicConsumer(channel);
+            //channel.BasicConsume(randomQueueName, false, consumer);
+            //Console.WriteLine("Logları dinleniyor... Çok yorulmuşlardı.");
+            //consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            //{
+            //   var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            //   Thread.Sleep(1500);
+            //   Console.WriteLine("Gelen Mesaj:" + message);
+            //   channel.BasicAck(e.DeliveryTag, false);
+            //}
+            #endregion
+
+            #region Direct Exchange
+            //channel.BasicQos(0, 1, false);
+            //var consumer = new EventingBasicConsumer(channel);
+            //var queueName = "direct-queue-Critical";
+            //channel.BasicConsume(queueName, false, consumer);
+            //Console.WriteLine("Logları dinleniyor... Çok yorulmuşlardı.");
+            //consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            //{
+            //   var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            //   Thread.Sleep(1500);
+            //   Console.WriteLine("Gelen Mesaj:" + message);
+            //Gelen mesaj log tutma
+            //   File.AppendAllText("log-critical.txt", message + "\n");
+            //   channel.BasicAck(e.DeliveryTag, false);
+            //}
+            #endregion
+
+            #region Topic Exchange
+            //channel.BasicQos(0, 1, false);
+            //var consumer = new EventingBasicConsumer(channel);
+            //Topic exchange da subscriber(yayınlanan) alanda hangileri alınacaksa o filtrelenir.
+            //var routeKey = "*.Error.*";
+            //var routeKey = "*.*.Warning";
+            //var routeKey = "Info.#";
+            //channel.QueueBind(queueName, "logs-topic", routeKey);
+            //channel.BasicConsume(queueName, false, consumer);
+            //Console.WriteLine("Logları dinleniyor... Çok yorulmuşlardı.");
+            //consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            //{
+            //   var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            //   Thread.Sleep(1500);
+            //   Console.WriteLine("Gelen Mesaj:" + message);
+            //   channel.BasicAck(e.DeliveryTag, false);
+            //}
+            #endregion
+
+            #region Header Exchange
+            //channel.BasicQos(0, 1, false);
+            //var consumer = new EventingBasicConsumer(channel);
+            //var queueName = channel.QueueDeclare().QueueName;
+            //Dictionary<string, object> headers = new Dictionary<string, object>();
+            //headers.Add("format", "pdf");
+            //headers.Add("shape", "a4");
+            //headers.Add("x-match", "any");--all tüm mesaj eşleşmesi, any herhangi bir mesaj eşleşmesi
+            //channel.QueueBind(queueName, "header-exchange", String.Empty, headers);
+            //channel.BasicConsume(queueName, false, consumer);
+            //Console.WriteLine("Logları dinleniyor... Çok yorulmuşlardı.");
+            //consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            //{
+            //   var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            //   Thread.Sleep(1500);
+            //   Console.WriteLine("Gelen Mesaj:" + message);
+            //   channel.BasicAck(e.DeliveryTag, false);
+            //}
+            #endregion
+
+            #region Header Exchange - Complex Type
+            channel.BasicQos(0, 1, false);
+            var consumer = new EventingBasicConsumer(channel);
+
+            var queueName = channel.QueueDeclare().QueueName;
+
+            Dictionary<string, object> headers = new Dictionary<string, object>();
+
+            headers.Add("format", "pdf");
+            headers.Add("shape", "a4");
+            headers.Add("x-match", "any");
+
+            channel.QueueBind(queueName, "header-exchange", String.Empty, headers);
+
+            channel.BasicConsume(queueName, false, consumer);
+
+            Console.WriteLine("Logları dinleniyor... Çok yorulmuşlardı.");
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                //Byte türünden gelen mesaj string ile karşılandı.
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Product product = JsonSerializer.Deserialize<Product>(message);
+
+                Thread.Sleep(1500);
+                Console.WriteLine($"Gelen Mesaj: {product.Id}-{product.Name}-{product.Price}-{product.Stock}");
+
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UdemyRabbitmq.publisher/Program.cs
@@ -0,0 +1,162 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Shared;
+using System.Text.Json;
+
+namespace UdemyRabbitmq.publisher
+{
+    #region Notlar
+    //Exchange: Var olan bir exchange varsa o yazılmalı, yoksa o kısım default exchange olarak ifade edilir.
+    //Routing Key: Belirlenen kuyruk ifade edilir. Default exchange kullanılıyorsa belirlenen kuyruk yazılır.
+    //Exchange tanımlı ise oradan gelen kuyruk ifade edilir.
+    //MessageBody: Byte türülen oluşturulan mesaj atanır.
+    //Durable: kuyruğun kayıt altına alınıp alınmayacağını belirler.
+    //True olursa kuyruk sistem yeniden başlatıldığında otomatik olarak silinir.
+    //False olursa sistemde fiziksel olarak kayıt edilip sistem yeniden başlatılsa dahi sistemden silinmez.
+    //Exclusive: Farklı process(library) arasında erişim sağlama durumunu belirler.
+    //True olursa sadece bulunduğu process(library) içinde çalışır.
+    //False olursa diğer process ile etkileşim içine girebilir.
+    #endregion
+
+    //Direct - Topic Exchange Log İsimleri
+    public enum LogNames
+    {
+        Critical = 1,
+        Error = 2,
+        Warning = 3,
+        Info = 4
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Bağlantı Kurma Aşaması
+            //RabbitMQ ile bağlantı kurulması için ConnectionFactory nesnesi eklendi.
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://zxuhuhnz:uwT3kF1kowQ8859E7O4WgNOon-iqc9VJ@tiger.rmq.cloudamqp.com/zxuhuhnz"); //AMQP URL alındı
+
+            //Eklenen nesne üzerinden RabbitMq Cloud hesabından AMQP URL alınarak bağlantı kuruldu.
+            using var connection = factory.CreateConnection();
+
+            //Oluşturulacak kuyruk/exchange için bir kanal(model) yaratıldı.
+            var channel = connection.CreateModel();
+            #endregion
+
+            //Kuyruk Oluşturma
+
+            #region Basit Kuyruk Oluşturma (Exchange Olmadan) 1
+            //channel.QueueDeclare("hello-queue", true, false, false);
+            //string message = "hello world";
+            //var messageBody = Encoding.UTF8.GetBytes(message);
+            //channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+            //Console.WriteLine("mesaj gönderilmiştir");
+            #endregion
+
+            #region Basit Kuyruk Oluşturma (Exchange Olmadan) 2
+            //channel.QueueDeclare("hello-queue", true, false, false);
+            //Enumerable.Range(1, 50).ToList().ForEach(x =>
+            //{
+            //    string message = $"Message {x}";
+            //    var messageBody = Encoding.UTF8.GetBytes(message);
+            //    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+            //    Console.WriteLine($"Mesaj gönderilmiştir : {message}");
+            //});
+            #endregion
+
+            //Exchange oluşturma
+
+            #region Fanout Exchange
+            //channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
+            //Enumerable.Range(1, 50).ToList().ForEach(x =>
+            //{
+            //    string message = $"log {x}";
+            //    var messageBody = Encoding.UTF8.GetBytes(message);
+            //    channel.BasicPublish("logs-fanout", "", null, messageBody);
+            //    Console.WriteLine($"Mesaj gönderilmiştir : {message}");
+            //});
+            #endregion
+
+            #region Direct Exchange
+            //channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);
+            //Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+            //{
+            //    var routeKey = $"route-{x}";
+            //    var queueName = $"direct-queue-{x}";
+            //    channel.QueueDeclare(queueName, true, false, false);
+            //    channel.QueueBind(queueName, "logs-direct", routeKey, null);
+            //});
+
+            //Enumerable.Range(1, 50).ToList().ForEach(x =>
+            //{
+            //    LogNames log = (LogNames)new Random().Next(1, 5);
+            //    string message = $"log-type: {log}";
+            //    var messageBody = Encoding.UTF8.GetBytes(message);
+            //    var routeKey = $"route-{log}";
+            //    channel.BasicPublish("logs-direct", routeKey, null, messageBody);
+            //    Console.WriteLine($"Log gönderilmiştir : {message}");
+            //});
+            #endregion
+
+            #region Topic Exchange
+            //channel.ExchangeDeclare("logs-topic", durable: true, type: ExchangeType.Topic);
+            //Random rnd = new Random();
+            //Enumerable.Range(1, 50).ToList().ForEach(x =>
+            //{
+            //    LogNames log1 = (LogNames)rnd.Next(1, 5);
+            //    LogNames log2 = (LogNames)rnd.Next(1, 5);
+            //    LogNames log3 = (LogNames)rnd.Next(1, 5);
+            //    var routeKey = $"{log1}.{log2}.{log3}";
+            //    string message = $"log-type: {log1}-{log2}-{log3}";
+            //    var messageBody = Encoding.UTF8.GetBytes(message);
+            //    channel.BasicPublish("logs-topic", routeKey, null, messageBody);  
+            //    Console.WriteLine($"Log gönderilmiştir : {message}");
+            //});
+            #endregion
+
+            #region Header Exchange
+            //channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);          
+            //Dictionary<string, object> headers = new Dictionary<string, object>();          
+            //headers.Add("format", "pdf");
+            //headers.Add("shape2", "a4"); --any durumuna uyan mesaj //headers.Add("shape", "a4"); --all durumuna uyan mesaj
+            //var properties = channel.CreateBasicProperties();
+            //properties.Headers = headers;
+            //channel.BasicPublish("header-exchange", string.Empty, properties, Encoding.UTF8.GetBytes("header mesajım"));
+            //Console.WriteLine("mesaj gönderilmiştir");
+            #endregion
+
+            #region Header Exchange - Complex Type
+            channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+
+            Dictionary<string, object> headers = new Dictionary<string, object>();
+
+            headers.Add("format", "pdf");
+            headers.Add("shape", "a4");
+
+            var properties = channel.CreateBasicProperties();
+            properties.Headers = headers;
+            //Persistent Mesajları kalıcı hale getirmeyi sağlar. Diğer exchangelar da kullanılabilir.
+            properties.Persistent = true;
+
+            var product = new Product
+            {
+                Id = 1,
+                Name = "Kalem",
+                Price = 100,
+                Stock = 10
+            };
+
+            var productJsonString = JsonSerializer.Serialize(product);
+
+            channel.BasicPublish("header-exchange", string.Empty, properties, Encoding.UTF8.GetBytes(productJsonString));
+
+            Console.WriteLine("mesaj gönderilmiştir");
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
